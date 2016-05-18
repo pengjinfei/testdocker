dec4c95536a72889c5d019fdd1f4fe70cde3bfe8
pengjinfei <pengjinfei@126.com>
2016-05-19 01:03:43 +0800

    test img

--- src/main/java/com/pengjinfei/servlet/TestServlet.java
+++ src/main/java/com/pengjinfei/servlet/TestServlet.java
@@ -8,12 +8,12 @@ import java.io.*;
  */
 public class TestServlet extends javax.servlet.http.HttpServlet {
     protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
-
+        doGet(request,response);
     }
 
     protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
-        String path = request.getContextPath() + "/WEB-INF";
-        File file = new File(path, "Aerial02.jpg");
+        String path = request.getServletContext().getRealPath("/WEB-INF/Aerial02.jpg");
+        File file = new File(path);
         InputStream in = new FileInputStream(file);
         BufferedOutputStream bout = new BufferedOutputStream(
                 response.getOutputStream());
